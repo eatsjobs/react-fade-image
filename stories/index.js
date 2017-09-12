8901305887448d617cd7bd59c376dc6db1590f3b
eatsjobs <pasqualemangialavori@docomodigital.com>
2017-09-12 09:50:02 +0200

    update stories

--- stories/index.js
+++ stories/index.js
@@ -7,21 +7,28 @@ import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 
 import { Button, Welcome } from '@storybook/react/demo';
 import FadeImage from '../src/index';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
-
 const fadeImageStoy = storiesOf('FadeImage', module)
 fadeImageStoy.addDecorator(withKnobs);
 fadeImageStoy.add('opacity', () => {
   const inline = { width: '50%', margin: '0 auto' };
-  const spacer = { width: '100%', display: 'block', height: '768px' };
+  const spacer = { width: '100%', display: 'block', height: '768px', };
   return (
-    <div>
-      <div style={spacer}> Scroll down to see the images loading</div>
+    <div style={{ margin: '0 auto', width: '60%' }}>
+      <h1 style={{ textAlign: 'center' }}>Welcome</h1>
+      <div style={spacer}>
+        This is a simple and little component for loading an image more smoothly
+        in you application even if you don't know the height.
+        You can use it if:
+        <ul>
+          <li> - You don't know the height of the image but the ratio</li>
+          <li>- You want to precalculate the image obstruction in your UI</li>
+          <li>- You want to lazy load the image as you scroll</li>
+        </ul>
+
+        You can also pass to it only the <strong>ratio</strong> string like this: '16:9' or '4:3' or any ratio you like.
+        <br />
+        <strong>Scroll down</strong> to see the images loading.
+      </div>
       <div style={inline}>
         <FadeImage src={text('Image src', 'https://placeimg.com/400/300/animals')}
           width={number('Width', 400)}
